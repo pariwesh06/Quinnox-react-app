15467b099e9b27ada1548107c431bd6fe9bebeec
pariwesh009 <ajay2322020@outlook.com>
2021-02-25 16:45:42 +0530

    final code for day-1

--- src/userform.js
+++ src/userform.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './userform.css';
 //ES6
 export default class Userform extends React.Component {
     constructor(props) {
@@ -6,10 +7,7 @@ export default class Userform extends React.Component {
         this.state = {
             firstname: this.props.firstname,
             age: this.props.age || 10,
-            users: [{
-                firstname: 'Pariwesh',
-                age: 10,
-            }]
+            users: []
         }
     }
     render = () => {
@@ -22,19 +20,33 @@ export default class Userform extends React.Component {
                 <button onClick={this.save}> Save</button>
                 <table>
                     <thead>
-                        <tr><th>firstname</th> <th>Age</th></tr>
+                        <tr><th>firstname</th><th>Age</th>
+                        <th>gender</th>
+                        <td></td></tr>
                     </thead>
                     <tbody>
                         {this.state.users.map((user, index) => {
                             return <tr>
                                 <td>{user.firstname}</td>
                                 <td>{user.age}</td>
+                                <td>{user.gender}</td>
+                                <td><button onClick={this.deleteRow.bind(this, index)}>Delete</button></td>
                             </tr>
                         })}
                     </tbody>
                 </table>
             </div>
         )
+    }
+    deleteRow=(selectedRow)=> {
+        if(!window.confirm('Are you sure?')){
+            return;
+        }
+        console.log(selectedRow);//splice
+        this.state.users.splice(selectedRow, 1);
+        this.setState({
+            users: this.state.users
+        });
     }
     handleEvent = (event) => {
         this.setState({ //tells react to update UI 
@@ -45,5 +57,13 @@ export default class Userform extends React.Component {
 
     save = () => {  //event handler for button
         console.log(this.state);
+        this.state.users.push({
+            firstname:this.state.firstname,
+            age:this.state.age,
+            gender:this.state.gender
+        });
+        this.setState({
+            users:this.state.users
+        })
     }
 } //ALT+SHIFT+F
